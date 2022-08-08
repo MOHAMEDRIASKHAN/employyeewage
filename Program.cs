ac56cb12c0625868ab9134a741773f109a1a4aed
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-08 19:29:50 +0530

    Greeting Messeage

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace employeewage1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //employeecheck.calculatingEmployeeWage();
+            //employeeuc5.calculatingEmployeeWage();
+            Employeeuc6.calculatingEmployeeWage();
+        }
+    }
+}
